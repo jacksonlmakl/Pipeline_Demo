5c1de1e78390235fe95002128325e953f4c37599
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-10-09 19:21:10 -0400

    Update Log.py

--- core/Log.py
+++ core/Log.py
@@ -1,35 +1,35 @@
-import logging
 import sys
-
-
-# Redirect print() to logging
+import os
+import logging
+import datetime
+# Define the PrintLogger class for capturing stdout and stderr
 class PrintLogger:
     def __init__(self, logger, level):
         self.logger = logger
         self.level = level
-    
-    def write(self, message):
-        if message.strip() != "":  # Ignore empty messages
-            self.logger.log(self.level, message)
-    
-    def flush(self):
-        pass  # Needed for compatibility with Python's file-like objects
 
+    def write(self, message):
+        if message.strip():  # Ignore empty messages
+            self.logger.log(self.level, message)
+
+    def flush(self):
+        pass  # For file-like object compatibility
 
 
 class Log:
-  def __init__(self,file_name):
-    global sys
-    import sys
-    global os
-    import os
-    global logging
-    import logging
-    # Redirect stdout and stderr to the logger
-  
-    # Configure logging to log to a file
-    logging.basicConfig(filename=f'{file_name}.log', level=logging.INFO, 
-                        format='%(asctime)s - %(levelname)s - %(message)s')
-    sys.stdout = PrintLogger(logging.getLogger(), logging.INFO)
-    sys.stderr = PrintLogger(logging.getLogger(), logging.ERROR)
-    print("Logger Status Check")
+    def __init__(self):
+        file_name=datetime.datetime.now().__str__().replace("-","_").replace(" ","__").replace(":","_").split(".")[0]
+        print(f"Saving log to {file_name}")
+        # Configure logging to log to a file
+        logging.basicConfig(
+            filename=f'log__{file_name}.log', 
+            level=logging.INFO, 
+            format='%(asctime)s - %(levelname)s - %(message)s'
+        )
+
+        # Redirect stdout and stderr to the logger
+        sys.stdout = PrintLogger(logging.getLogger(), logging.INFO)
+        sys.stderr = PrintLogger(logging.getLogger(), logging.ERROR)
+
+        # Test the logger by printing a message
+        print("Logger Status Check")
