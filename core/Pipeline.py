019fc5b76f6a8a6344c870562b5afa195b6a0932
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-10-09 19:29:28 -0400

    Update Pipeline.py

--- core/Pipeline.py
+++ core/Pipeline.py
@@ -1,10 +1,48 @@
 from core import Task
 from core import Connection
 from core import Table
-from core import PipelineLogger
 import re
 from jinja2 import Template
 import json
+import sys
+import os
+import logging
+import datetime
+
+# Define the PrintLogger class for capturing stdout and stderr
+class PrintLogger:
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+
+    def write(self, message):
+        if message.strip():  # Ignore empty messages
+            self.logger.log(self.level, message)
+
+    def flush(self):
+        pass  # For file-like object compatibility
+
+
+class PipelineLogger:
+    def __init__(self):
+        file_name=datetime.datetime.now().__str__().replace("-","_").replace(" ","__").replace(":","_").split(".")[0]
+        print(f"Saving log to {file_name}.log")
+        # Configure logging to log to a file
+        logging.basicConfig(
+            filename=f'log__{file_name}.log', 
+            level=logging.INFO, 
+            format='%(asctime)s - %(levelname)s - %(message)s'
+        )
+
+        # Redirect stdout and stderr to the logger
+        sys.stdout = PrintLogger(logging.getLogger(), logging.INFO)
+        sys.stderr = PrintLogger(logging.getLogger(), logging.ERROR)
+
+        # Test the logger by printing a message
+        print("Logger Status Check")
+        
+        
+
 
 def xml(xml_string):
     # Load your variables.json file
